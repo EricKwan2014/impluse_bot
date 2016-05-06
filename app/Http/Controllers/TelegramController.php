d55818016d84fd4e0904c38faaef435e1ded5e2d
EricKwan <eric@bull-b.com>
2016-05-06 11:43:57 +0800

    update

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Validator;
+use willvincent\Feeds\Facades\FeedsFacade as Feeds;
 use Telegram\Bot\Api;
 use Input;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Request;
 use Telegram\Bot\Objects\Message;
+use Telegram\Bot\Actions;
 
 class TelegramController extends Controller
 {
@@ -21,6 +23,26 @@ class TelegramController extends Controller
 
     public function getUpdates()
     {
+        $disasters_feed = Feeds::make('http://www.who.int/feeds/entity/csr/don/zh/rss.xml', true);
+        $disasters_news = $disasters_feed->get_items()[0]->get_description();
+
+
+        $feed = Feeds::make('http://rss.weather.gov.hk/rss/CurrentWeather_uc.xml', true);
+        $data = array(
+          // 'title'     => $feed->get_title(),
+          // 'permalink' => $feed->get_permalink(),
+          'items'     => $feed->get_items(),
+        );
+        // $response = $this->telegram->sendMessage([
+        //   'chat_id' => '-147748587', 
+        //   'text' => $feed->data['items'][0]->data['child']['']['description'][0]['data']
+        // ]);
+        $this->telegram->sendChatAction([
+          'chat_id' => '-147748587',
+          'action' => Actions::RECORD_VIDEO
+        ]);
+        return $this->telegram->getUpdates();
+
         $updates = $this->telegram->getUpdates();
         $message = collect($updates)->last()->getMessage();
         $chatId = $message->getChat()->getId();
@@ -63,8 +85,25 @@ class TelegramController extends Controller
         $sender = $message->getFrom()->getUsername();
         $text = $message->getText();
         $str = substr($text, 1);
-        $str = strstr($str, '@', true);
+        $debug = strstr($str, '@', true);
 
+        $disasters_feed = Feeds::make('http://www.who.int/feeds/entity/csr/don/zh/rss.xml', true);
+        $disasters_news = $disasters_feed->get_items()[0]->get_description();
+
+        $reminder_help = "/remind (用家) (時間) (動作)\n e.g /remind Eric tomorrow_3pm take_drug";
+
+        if($str=='disasters_news'){
+            $response = $this->telegram->sendMessage([
+              'chat_id' => $chatId,
+              'text' => $disasters_news
+            ]);
+        }
+        if($str=='reminder_help'){
+            $response = $this->telegram->sendMessage([
+              'chat_id' => $chatId,
+              'text' => $reminder_help
+            ]);
+        }
         if($str=='oscar' || $str=='silver'){
             $response = $this->telegram->sendMessage([
               'chat_id' => $chatId,
--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use willvincent\Feeds\Facades\FeedsFacade as Feeds;
 use App\Http\Requests;
+use Illuminate\Support\Facades\View;
 
 class WeatherController extends Controller
 {
-    //
+    public function index() {
+    	$json = json_decode(file_get_contents('http://api.openweathermap.org/v3/uvi/22.3,114.2/'.date('Y-m-d').'Z.json?appid='.config('app.weather_token')), true);
+    	$feed = Feeds::make('http://www.who.int/feeds/entity/csr/don/zh/rss.xml', true);
+    	$data = array(
+	      // 'title'     => $feed->get_title(),
+	      // 'permalink' => $feed->get_permalink(),
+	      'items'     => $feed->get_items(),
+	    );
+	    $total = $feed->get_items()[0]->get_description();
+	    $part_a = $total;
+	    return $total;
+    	// return dd($feed->data['items'][0]->data['child']['']['description'][0]['data']);
+	    // return View::make('feeds', $data);
+    }
 }
